3a6eda4e21a1c4ff2597f9d15cd5056db60a4b08
lucsimao <lucasimao64@gmail.com>
2022-02-20 20:30:49 -0300

    refactor: update remaining styles and using media query

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -8,10 +8,15 @@ const Container = styled.div`
   min-height: 90vh;
   padding: 0px 15vw;
 `;
-const Conteudo = styled.session`
+
+const Conteudo = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
 `;
 
 export default () => {
