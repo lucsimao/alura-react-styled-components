380d0e0822f95b16c80cadf780e9504ab3e67fea
lucsimao <lucasimao64@gmail.com>
2022-02-21 21:33:19 -0300

    feat: add functionality to theme button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
-import { temaClaro } from "./Components/UI/temas";
+import SwitchTema from "./Components/SwitchTema";
+import { BtnTema } from "./Components/UI";
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
+
 function App() {
+  const [tema, setTema] = useState(true);
+  const toggleTema = () => {
+    setTema((tema) => !tema);
+  };
+
   return (
     <>
-      <ThemeProvider theme={temaClaro}>
+      <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
         <GlobalStyle />
+        <BtnTema onClick={toggleTema}>
+          <SwitchTema tema={tema} />
+        </BtnTema>
         <Cabecalho />
         <Container />
       </ThemeProvider>
--- src/Components/SwitchTema/index.jsx
+++ src/Components/SwitchTema/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import {
-  default as ThemeOff,
-  default as ThemeOn,
-} from "../../assets/images/themeOn.svg";
+import ThemeOff from "../../assets/images/themeOff.svg";
+import ThemeOn from "../../assets/images/themeOn.svg";
 import { Icone } from "../UI";
 
 const claro = <Icone src={ThemeOn} alt="Tema Claro" />;
